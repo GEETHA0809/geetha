a51e84823b290a717c6639ceb8845344541fb9a8
GEETHA0809 <pithanigeetha@gmail.com>
2024-11-12 21:06:59 -1200

    Add files via upload

---
+++ hhhhh.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int top;
+    printf("Enter any key:");
+    scanf("%d",top);
+    printf("HELLO WORLD!!!!!!\n");
+    printf("HELLO WORLD!!!!!!\n");
+    printf("HELLO WORLD!!!!!!\n");
+    printf("HELLO WORLD!!!!!!\n");
+    printf("HELLO WORLD!!!!!!\n");
+    printf("HELLO WORLD!!!!!!\n");
+    return 0;
+
+}
